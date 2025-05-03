4cf61d88c2a74f6bc3aa959fac2c0e0221907c0a
shirbhate-amit <amit.shirbhate@calsoftinc.com>
2025-05-03 18:17:52 +0530

    Update app.py

--- Parameterised CICD Jenkins/app.py
+++ Parameterised CICD Jenkins/app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Get parameter from command-line or use default
-    message = sys.argv[1] if len(sys.argv) > 1 else "Default Message"
-    return message
+    param = sys.argv[1] if len(sys.argv) > 1 else "default"
+    return f"Hello from Jenkins! Received param: {param}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
